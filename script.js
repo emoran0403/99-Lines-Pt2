2833b9bdde03979f1dd5dca5dc0f32e8272679ad
Eric Moran <emoran0403@gmail.com>
2022-01-14 09:58:39 -0500

    cleaned up grammar and logic in console logs

--- script.js
+++ script.js
@@ -13,13 +13,16 @@ function song() { //this code will run when the button is clicked
     for (var j = 0; j < friends.length; j++) {
         let current = friends[j]
         for (let i = 99; i > 0; i--) {
-            if (i > 1) {
-                console.log(i + " lines of code in the file, " + i + " lines of code; " + current + " strikes one out, clears it all out, " + i + " lines of code in the file");
-            }
-            if (i == 1) {
-                console.log(i + " line of code in the file, " + i + " line of code; " + current + " strikes one out, clears it all out, " + i + " line of code in the file");
+            if (i === 1) {
+                //console.log(i + " line of code in the file, " + i + " line of code; " + current + " strikes it out, clears it all out, " + (i - 1) + " lines of code in the file");
+                console.log(`${i} line of code in the file, ${i} line of code, ${current} strikes it out, cleares it all out, ${i-1} lines of code in the file`)
+            } else if (i === 2) {
+                //console.log(i + " lines of code in the file, " + i + " lines of code; " + current + " strikes one out, clears it all out, " + (i - 1) + " line of code in the file");
+                console.log(`${i} lines of code in the file, ${i} lines of code, ${current} strikes one out, cleares it all out, ${i-1} line of code in the file`)
+            } else {
+                //console.log(i + " lines of code in the file, " + i + " lines of code; " + current + " strikes one out, clears it all out, " + (i - 1) + " lines of code in the file");
+                console.log(`${i} line of code in the file, ${i} line of code, ${current} strikes one out, cleares it all out, ${i-1} lines of code in the file`)
             }
         }
-    }
-
+    }   
 }
